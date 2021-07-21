1d3f5844a177dd6fd81e698539bfb31e08111e5a
jeni <s7130457@gmail.com>
2021-07-21 21:39:33 +0800

    dev: 因為是line的api壞掉，所以把test的跟他對調

--- api/index.js
+++ api/index.js
@@ -7,12 +7,13 @@ router.use('/', (req, res) => {
   res.send('<h1>server is running</h1>');
 })
 
-router.use('/line', lineRoutes)
-
 router.use('/test', (req, res) => {
   console.log('hihihi');
   return res.status(200).json(123)
 })
+
+router.use('/line', lineRoutes)
+
 
 
 module.exports = router
